cae2d19ab077d026f253f547d9fbbf61502abd9b
Eric Drake <puturosule@gmail.com>
2022-11-10 12:40:07 -0700

    finished isPalindrome function

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,26 @@
 // Write your code below
+function isPalindrome(word){
+    let b = word.length - 1
+    for(let i = 0; i < word.length; i++){
+        if(word[i] != word[b--]){
+            return false
+        }
+    }
+    return true
+}
+
+//console.log(isPalindrome('HannaH'));
+
+function isPalindrome2(word)
+{
+    for(let i = 0, b = word.length - 1; i <= b; i++, b--)
+    {
+        if(word[i] !== word[b])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPalindrome2('racecart'));
